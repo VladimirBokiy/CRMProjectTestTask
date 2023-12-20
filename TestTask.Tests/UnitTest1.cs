9ab953acdb6e41808cf5abcd80553579d7b93531
VladimirBokiy <vl.bokiy@gmail.com>
2023-12-20 17:00:08 +0300

    add unit tests

---
+++ TestTask.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using TestTask.Services;
+
+namespace TestTask.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
